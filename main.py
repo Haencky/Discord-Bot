de3da78b8c6a33b82240e210abfab0cf62ff3071
Tim <schulztimole@gmail.com>
2025-03-29 02:59:23 +0100

    added status and activity

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.messages = True
 intents.polls = True
-bot = ezcord.Bot(intents=intents, debug_guilds=[1145030840401793104])
+status = discord.Status.online
+acvtivity = discord.Activity(type=discord.ActivityType.watching, name='Geeky Stuff')
+bot = ezcord.Bot(
+    intents=intents,
+    debug_guilds=[1145030840401793104],
+    status=status,
+    activity=acvtivity,
+    )
 
 if __name__ == '__main__':
     bot.load_cogs('cogs')
